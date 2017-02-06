9743840e6b4a5db40d058073ae38cba0f60b9e94
trapkingwillie <babcock.brendan@gmail.com>
2017-02-06 13:17:48 -0500

    6FEB16

--- digital_assignment_1/js/main.js
+++ digital_assignment_1/js/main.js
@@ -18,7 +18,9 @@ window.onload = function() {
     function preload() {
         game.load.image( 'astronaut', 'assets/astronaut.png', 150, 150 );
         game.load.image('earth', 'assets/earth.jpg', 800, 600);
-        game.load.image('asteroids', 'assets/asteroid.png', 50, 50);
+        game.load.image('asteroid1', 'assets/asteroid.png', 50, 50);
+        game.load.image('asteroid2', 'assets/asteroid.png', 50, 50);
+        game.load.image('asteroid3', 'assets/asteroid.png', 50, 50);
         game.load.audio('theme', 'assets/digital_assignment_1_music.mp3');
     }
     
@@ -30,8 +32,19 @@ window.onload = function() {
     var earth;
     var theme;
     var loopControl;
-    var asteroids;
-    var countdown;
+    var asteroid1;
+    var asteroid2;
+    var asteroid3;
+    var position1 = (Math.random()*450);
+    var position2 = (Math.random()*400);
+    var position3 = (Math.random()*600);
+    var position4 = (Math.random()*320);
+    var position5 = (Math.random()*150);
+    var position6 = (Math.random()*200);
+    var gameOver = 0;
+    var message = 0;
+    var asteroidHit = 0;
+
     
     function create() {
 
@@ -41,45 +54,107 @@ window.onload = function() {
         game.physics.arcade.gravity.x = (Math.random()*150);
         earth = game.add.sprite( game.world.centerX, game.world.centerY, 'earth' );
         astronaut = game.add.sprite( game.world.centerX, game.world.centerY, 'astronaut' );
+        asteroid1 = game.add.sprite(position1, position2, 'asteroid1');
+        asteroid2 = game.add.sprite(position3, position4, 'asteroid2');
+        asteroid3 = game.add.sprite(position5, position6, 'asteroid3');
         earth.anchor.setTo(0.5, 0.5);
         astronaut.anchor.setTo( 0.5, 0.5 );
         astronaut.scale.setTo(0.5,0.5);
+        asteroid1.scale.setTo(0.25,0.25);
+        asteroid2.scale.setTo(0.25,0.25);
+        asteroid3.scale.setTo(0.25,0.25);
         astronaut_up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
         astronaut_down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
         astronaut_right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
         astronaut_left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
 
-        countdown = game.time.create(true);
-        countdown.loop((Math.random()*75000));
-        countdown.start();
-        asteroids = game.add.group()
-        asteroids.scale.setTo(0.25,0.25);
-        for(loopControl=0; loopControl<(Math.random()*77); loopControl++)
-        {
-            asteroids.create((Math.random()*1000), (Math.random()*1800), 'asteroids');
-        }
+        // countdown = game.time.create(false);
+        // countdown.loop((Math.random()*30000), iterationsOfCounter, this);
+        // countdown.start();
+        game.time.events.add(Phaser.Timer.SECOND*(Math.random()*100), stopTimer, this);
 
-        game.physics.enable([asteroids, astronaut], Phaser.Physics.ARCADE);
-
-
+        game.physics.enable([asteroid1, asteroid2, asteroid3, astronaut], Phaser.Physics.ARCADE);
         theme = game.add.audio('theme');
         theme.play();
 
-        
+        astronaut.body.onCollide = new Phaser.Signal();
+        astronaut.body.onCollide.add(asteroidCollision, this);
+        asteroid1.body.onCollide = new Phaser.Signal();
+        asteroid1.body.onCollide.add(asteroidCollision, this);
+        asteroid2.body.onCollide = new Phaser.Signal();
+        asteroid2.body.onCollide.add(asteroidCollision, this);
+        asteroid3.body.onCollide = new Phaser.Signal();
+        asteroid3.body.onCollide.add(asteroidCollision, this);
 
         var style = { font: "25px Times New Roman", fill: "#ffffff", align: "right" };
-        var text = game.add.text( 675, 15, "Digital Assignment 1", style );
+        var text = game.add.text( 675, 15, "Space Escape", style );
         text.anchor.setTo( 0.5, 0.0 );
+
+    }
+
+    function asteroidCollision()
+    {
+        asteroidHit = 1;
+    }
+
+    function stopTimer()
+    {
+        gameOver = 1;
+        
     }
 
     function render() 
     {
-        game.debug.text('Stay on the map and avoid asteroids for: '+ countdown.duration, 32, 32);
+        // game.debug.text('Stay on the map and avoid asteroids for: '+ countdown.duration, 32, 32);
+        // game.debug.text('You have survived '+iterationsOfCounter+' iterations of this game. Good job!', 32, 56);
+        game.debug.text('Avoid asteroids and keep Astronaut John on the game map for: '+game.time.events.duration, 32, 64);
 
+        if (asteroidHit===1)
+        {
+            game.debug.text('Game Over, you hit an asteroid!!', 300, 300);        
+        }
+
+        if(astronaut.x>800 && gameOver ===0 && asteroidHit ===0) 
+        {
+            game.debug.text('Game Over, you went off the map!', 300, 300);
+            message = 1;
+        }
+
+        else if (astronaut.x<0 && gameOver ===0 && asteroidHit ===0)
+        {
+            game.debug.text('Game Over, you went off the map!', 300, 300);
+            message = 1;
+        }
+
+        else if(astronaut.y>600 && gameOver ===0 && asteroidHit ===0)
+        {
+            game.debug.text('Game Over, you went off the map!', 300, 300);
+            message = 1;
+        }
+
+        else if (astronaut.y<0 && gameOver ===0 && asteroidHit ===0)
+        {
+            game.debug.text('Game Over, you went off the map!', 300, 300);
+            message = 1;
+        }
+
+
+        if(gameOver ===1 && message === 0 && asteroidHit ===0) 
+        {
+           game.debug.text('Game Over! You win!', 400, 400);
+        }
 
     }
     
     function update() {
+        
+game.physics.arcade.collide(astronaut, asteroid1);
+game.physics.arcade.collide(astronaut, asteroid2);
+game.physics.arcade.collide(astronaut, asteroid3);
+
+
+        if(message===0 && gameOver === 0 && asteroidHit ===0) 
+        {
 
         if(astronaut_up.isDown) 
             {
@@ -97,6 +172,7 @@ window.onload = function() {
             {
                 astronaut.x+=50;
             }
+        }
       
     }
 };
