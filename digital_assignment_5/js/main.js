12c4a207a9c6ed779e39925f8e5eaf8d2640a2ef
trapkingwillie <babcock.brendan@gmail.com>
2017-03-04 18:29:01 -0500

    4MAR17 Push

--- digital_assignment_5/js/main.js
+++ digital_assignment_5/js/main.js
@@ -6,10 +6,15 @@ window.onload = function() {
     function preload() {
         game.load.image( 'landscape', 'assets/landscape.jpg', 150, 150 );
         game.load.image( 'bomb', 'assets/bomb.png', 150, 150 );
+        game.load.image('bomb2', 'assets/bomb.png', 150, 150);
         game.load.image( 'red', 'assets/redButton.png', 150, 150 );
         game.load.image( 'blue', 'assets/blueButton.png', 150, 150 );
         game.load.image( 'green', 'assets/greenButton.png', 150, 150 );
         game.load.image( 'table', 'assets/table.png', 150, 150 );
+        game.load.image('battery', 'assets/battery.png', 150, 150);
+        game.load.image('robot', 'assets/robot.png', 150, 150);
+        game.load.image('water', 'assets/water.png', 150, 150);
+        game.load.image('caesar', 'assets/caesar.png', 150, 150);
         game.load.audio('explosion', 'assets/explosion.mp3');
         game.load.audio('timer', 'assets/ticking.mp3');
         game.load.audio('snip', 'assets/snip.mp3');
@@ -25,21 +30,36 @@ window.onload = function() {
     var bombDiffusalAttempt = false;
     var explosion;
     var failed = false;
-    var messageNumber = Math.floor(Math.random()*32);
+    var messageNumber = Math.floor(Math.random()*32);//controls first bomb message
+    var filterNumber = Math.floor(Math.random()*32);//controls second bomb message
     var letter;
     var button;
     var diffused = false;
     var timer;
     var table;
     var snip;
+    var bomb2;
+    var table2;
+    var battery;
+    var robot;
+    var water;
+    var diffused2 = false;
+    var bomb2DiffusalAttempt = false;
+    var button2;
+    var caesar;
+    var helpRequired = false;
 
     
     function create() {
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
         landscape = game.add.sprite(0,0,'landscape');
-        bomb = game.add.button(670, 425, 'bomb', bombDiffusal, this, 2, 1, 0);
-        bomb.scale.setTo(1, 1);
+        bomb = game.add.button(100, 310, 'bomb', bombDiffusal, this, 2, 1, 0);
+        bomb2 = game.add.button(415, 310, 'bomb2', bomb2Diffusal, this, 2, 1, 0);
+        caesar = game.add.button(500, 310, 'caesar', help, this, 2, 1, 0);
+        caesar.scale.setTo(0.5, 0.5);
+        bomb.scale.setTo(0.35, 0.35);
+        bomb2.scale.setTo(0.45, 0.45);
         explosion = game.add.audio('explosion');
         timer = game.add.audio('timer');
         snip = game.add.audio('snip');
@@ -57,22 +77,40 @@ window.onload = function() {
 
     }
 
-    function bombDiffusal()
+    function help()
     {
+        helpRequired = true;
+    }
+
+    function bombDiffusal()//use for bomb1
+    {
+            helpRequired = false;
             bombDiffusalAttempt = true;
             game.time.events.add(Phaser.Timer.SECOND * 60, failedDiffusal, this);
-            table = game.add.sprite(300, 450, 'table');
-            table.scale.setTo(0.75, 0.75);
-            redButton = game.add.button(365, 515, 'red', redWireButton, this, 2, 1, 0);
+            table = game.add.sprite(-20, 450, 'table');
+            table.scale.setTo(1.75, 0.75);
+            redButton = game.add.button(160, 515, 'red', redWireButton, this, 2, 1, 0);
             redButton.scale.setTo(0.45, 0.45);
-            blueButton = game.add.button(440, 515, 'blue', blueWireButton, this, 2, 1, 0);
+            blueButton = game.add.button(230, 515, 'blue', blueWireButton, this, 2, 1, 0);
             blueButton.scale.setTo(0.38, 0.38);
-            greenButton = game.add.button(515, 515, 'green', greenWireButton, this, 2, 1, 0);
+            greenButton = game.add.button(300, 515, 'green', greenWireButton, this, 2, 1, 0);
             greenButton.scale.setTo(0.45, 0.45);
+            robot = game.add.button(400, 515, 'robot', robotButton, this, 2, 1, 0);
+            robot.scale.setTo(0.045, 0.045);
+            water = game.add.button(500, 515, 'water', waterButton, this, 2, 1, 0);
+            water.scale.setTo(0.030, 0.030);
+            battery = game.add.button(600, 540, 'battery', batteryButton, this, 2, 1, 0);
+            battery.scale.setTo(0.09, 0.09);
             timer.loop = true;
             timer.play();
 
 
+    }
+
+    function bomb2Diffusal()//used for bomb2
+    {
+        bomb2DiffusalAttempt = true;
+        helpRequired = false;
     }
 
     function greenWireButton()
@@ -117,9 +155,51 @@ window.onload = function() {
 
     }
 
+        function batteryButton()
+    {
+        snip.play();
+        if(button2==='battery')
+        {
+            diffused2 = true;
+        }
+        else
+        {
+            failedDiffusal();
+        }
+
+    }
+
+        function waterButton()
+    {
+        snip.play();
+        if(button2==='water')
+        {
+            diffused2 = true;
+        }
+        else
+        {
+            failedDiffusal();
+        }
+
+    }
+
+        function robotButton()
+    {
+        snip.play();
+        if(button2==='robot')
+        {
+            diffused2 = true;
+        }
+        else
+        {
+            failedDiffusal();
+        }
+
+    }
+
     function failedDiffusal()
     {
-        if(diffused===false)
+        if(diffused===false || diffused2===false)
         {
         explosion.play()
         failed = true;
@@ -130,13 +210,18 @@ window.onload = function() {
 
     function render() 
     {
-        if(bombDiffusalAttempt===true)
+        if(helpRequired===true && (bombDiffusalAttempt===false || bomb2DiffusalAttempt===false))
+        {
+            game.debug.text('A Caesar Cipher requires the message to be moved forwards in the alphabet,', 5,20);
+            game.debug.text('based upon the number. For instance, ROT1 of "hi" is "ij".', 5, 35);
+        }
+        if(bombDiffusalAttempt===true && bomb2DiffusalAttempt===true)
         {
             //the solution to the equation equals the letter in the message corresponding to a button.
-            if(diffused===false && failed===false)
+            if((diffused===false || diffused2===false) && failed===false)
             {
                 
-            game.debug.text('Diffuse the bomb in: '+game.time.events.duration, 32, 64);
+            game.debug.text('Diffuse the bombs in: '+game.time.events.duration, 32, 64);
             if(messageNumber===1&& failed===false)
             {
             game.debug.text('Message: Rarely good boys behave badly', 32, 96);
@@ -350,17 +435,214 @@ window.onload = function() {
                button = 'red'; 
 
             }
+
+            //start bomb2 messages here, dependent upon the second random variable. Bombs must be 
+            //diffused in a sequence - from right to letf. Buttons to diffuse are clicked 1, 2.
+
+            if(filterNumber===1 && failed===false)
+            {
+                game.debug.text('Message 2: xbufs', 32, 146);
+                game.debug.text('Filter: (2x-2)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===2 && failed===false)
+            {
+                game.debug.text('Message 2: tqdqv', 32, 146);
+                game.debug.text('Filter: (x-2)=0', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===3 && failed===false)
+            {
+                game.debug.text('Message 2: cbuufsz', 32, 146);
+                game.debug.text('Filter: (x^4-1)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===4 && failed===false)
+            {
+                game.debug.text('Message 2: dcvvgta', 32, 146);
+                game.debug.text('Filter: (x-2)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===5 && failed===false)
+            {
+                game.debug.text('Message 2: qnans', 32, 146);
+                game.debug.text('Filter: (x-25)=0', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===6 && failed===false)
+            {
+                game.debug.text('Message 2: aexiv', 32, 146);
+                game.debug.text('Filter: (x^(1/2)-2)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===7 && failed===false)
+            {
+                game.debug.text('Message 2: ycvgt', 32, 146);
+                game.debug.text('Filter: (x^2-4)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===8 && failed===false)
+            {
+                game.debug.text('Message 2: fexxivc', 32, 146);
+                game.debug.text('Filter: (x^3-64)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===9 && failed===false)
+            {
+                game.debug.text('Message 2: herej', 32, 146);
+                game.debug.text('Filter: (4x-48)=0', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===10 && failed===false)
+            {
+                game.debug.text('Message 2: ebobg', 32, 146);
+                game.debug.text('Filter: (x^2-169)=0', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===11 && failed===false)
+            {
+                game.debug.text('Message 2: gfyyjwd', 32, 146);
+                game.debug.text('Filter: (x-5)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===12 && failed===false)
+            {
+                game.debug.text('Message 2: fjcna', 32, 146);
+                game.debug.text('Filter: (x^2-81)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===13 && failed===false)
+            {
+                game.debug.text('Message 2: eibmz', 32, 146);
+                game.debug.text('Filter: (x^(1/3)-2)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===14 && failed===false)
+            {
+                game.debug.text('Message 2: qpiitgn', 32, 146);
+                game.debug.text('Filter: (x^2-225)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===15 && failed===false)
+            {
+                game.debug.text('Message 2: livin', 32, 146);
+                game.debug.text('Filter: (x^2-400)=0', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===16 && failed===false)
+            {
+                game.debug.text('Message 2: gkdob', 32, 146);
+                game.debug.text('Filter: (x^3-1000)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===17 && failed===false)
+            {
+                game.debug.text('Message 2: vunnyls', 32, 146);
+                game.debug.text('Filter: (x^2-400)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===18 && failed===false)
+            {
+                game.debug.text('Message 2: vsfsx', 32, 146);
+                game.debug.text('Filter: (x^2-16)=0', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===19 && failed===false)
+            {
+                game.debug.text('Message 2: hgzzkxe', 32, 146);
+                game.debug.text('Filter: (x-6)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===20 && failed===false)
+            {
+                game.debug.text('Message 2: jgtgl', 32, 146);
+                game.debug.text('Filter: (x-18)=0', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===21 && failed===false)
+            {
+                game.debug.text('Message 2: zyrrcpw', 32, 146);
+                game.debug.text('Filter: (x-24)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===22 && failed===false)
+            {
+                game.debug.text('Message 2: khuhm', 32, 146);
+                game.debug.text('Filter: (x-19)=0', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===23 && failed===false)
+            {
+                game.debug.text('Message 2: qunyl', 32, 146);
+                game.debug.text('Filter: (x^2-400)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===24 && failed===false)
+            {
+                game.debug.text('Message 2: lpitg', 32, 146);
+                game.debug.text('Filter: (x-15)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===25 && failed===false)
+            {
+                game.debug.text('Message 2: kjccnah', 32, 146);
+                game.debug.text('Filter: (9x-81)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===26 && failed===false)
+            {
+                game.debug.text('Message 2: rqjjuho', 32, 146);
+                game.debug.text('Filter: (x^2-256)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===27 && failed===false)
+            {
+                game.debug.text('Message 2: vzsdq', 32, 146);
+                game.debug.text('Filter: (x^(1/2)-5)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===28 && failed===false)
+            {
+                game.debug.text('Message 2: ptmxk', 32, 146);
+                game.debug.text('Filter: (x-19)=0', 32, 164);
+                button2 = 'water';
+            }
+            if(filterNumber===29 && failed===false)
+            {
+                game.debug.text('Message 2: bylyd', 32, 146);
+                game.debug.text('Filter: (x^2-100)=0', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===30 && failed===false)
+            {
+                game.debug.text('Message 2: axkxc', 32, 146);
+                game.debug.text('Filter: (x^2-81)', 32, 164);
+                button2 = 'robot';
+            }
+            if(filterNumber===31 && failed===false)
+            {
+                game.debug.text('Message 2: azssdqx', 32, 146);
+                game.debug.text('Filter: (x^(1/2)-5)=0', 32, 164);
+                button2 = 'battery';
+            }
+            if(filterNumber===32 && failed===false)
+            {
+                game.debug.text('Message 2: ihaalyf', 32, 146);
+                game.debug.text('Filter: (x-7)=0', 32, 164);
+                button2 = 'battery';
+            }
+
         }
 
 
         }
-        if(failed===true && diffused===false)
+        if(failed===true && (diffused===false || diffused2===false))
         {
             game.debug.text('You fail; game over.', 32, 114);
             timer.mute = true;
 
         }
-        if(diffused===true && failed===false)
+        if(diffused===true && diffused2===true && failed===false)
         {
             game.debug.text('You win - the rainforest is saved!', 32, 114);
             timer.mute = true;
